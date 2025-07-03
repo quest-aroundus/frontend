cbbca60bba023e52e1962e8b2b2d43e9b6dc5130
EunJin Kim <eunjin9639@gmail.com>
2025-07-02 21:46:12 -0400

    ✨ feat: update global styles and layout to improve font handling and color variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,6 @@ import "./globals.css";
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "AROUNDUS",
   description: "Aroundus is a social media platform that connects people through shared interests and activities.",
@@ -26,9 +16,7 @@ const Layout = ({
 }>) => {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Header />
         {children}
         <Footer />
