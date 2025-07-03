c5d3d0801cdca8b989d7d6bacec165d917e77cd3
allyes13 <kimallyes13@gmail.com>
2025-07-03 13:32:21 -0400

    feat: update Header component by adding icons and modify design

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,41 @@
+import AlarmIcon from "@/app/_assets/AlarmIcon";
+import Logo from "@/app/_assets/Logo";
+import SettingsIcon from "@/app/_assets/SettingsIcon";
+import Link from "next/link";
+
+const HeaderButton = ({ type }: { type: "alarm" | "settings" | "main" }) => {
+  const link = {
+    alarm: {
+      href: "/alarm",
+      icon: <AlarmIcon />,
+    },
+    settings: {
+      href: "/settings",
+      icon: <SettingsIcon />,
+    },
+    main: {
+      href: "/",
+      icon: <Logo />,
+    },
+  };
+
+  return (
+    <Link href={link[type].href} className="flex items-center cursor-pointer">
+      {link[type].icon}
+    </Link>
+  );
+};
+
 const Header = () => {
   return (
-    <header>
-      header
+    <header className="sticky w-full min-w-80 top-0 px-5 pt-[3.125rem] pb-[0.625rem]">
+      <div className="max-w-[35rem] mx-auto flex justify-between items-center">
+        <HeaderButton type="alarm" />
+        <HeaderButton type="main" />
+        <HeaderButton type="settings" />
+      </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
