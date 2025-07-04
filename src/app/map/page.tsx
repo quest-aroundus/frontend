2514d0db2a2714d13121c26aac69209665f4b026
EunJin Kim <eunjin9639@gmail.com>
2025-07-04 00:48:11 -0400

    ✨ feat: add MapboxMap component and integrate it into MapPage

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,5 +1,14 @@
+import dynamic from 'next/dynamic';
+
+// 클라이언트 전용 Mapbox 컴포넌트 불러오기
+const MapboxMap = dynamic(() => import('@/components/MapboxMap'));
+
 const MapPage = () => {
-  return (<main>map</main>)
+  return (
+    <main>
+      <MapboxMap latitude={43.6532} longitude={-79.3832} />
+    </main>
+  );
 }
 
-export default MapPage;
+export default MapPage
