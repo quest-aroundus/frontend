b957ebc2084509eb1caf3980cce7811ed10d92e8
allyes13 <kimallyes13@gmail.com>
2025-07-07 18:44:13 -0400

    ✨ feat: add FilterIcon, MagnifyIcon, and PlusIcon components

---
+++ src/app/_assets/PlusIcon.tsx
@@ -0,0 +1,26 @@
+const PlusIcon = () => {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15 7.5L15 22.5"
+        stroke="currentColor"
+        strokeWidth="2.144"
+        strokeLinecap="round"
+      />
+      <path
+        d="M7.5 14.957L22.5 14.957"
+        stroke="currentColor"
+        strokeWidth="2.144"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
+
+export default PlusIcon;
