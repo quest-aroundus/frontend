a3c8fbc6a87289a6e66026dd3eb446ca6bdffce3
EunJin Kim <eunjin9639@gmail.com>
2025-07-15 21:56:27 -0400

    ✨ feat: 개선된 로딩 처리 및 오류 메시지 추가; MapPage에서 MapSkeleton을 사용하여 사용자 경험 향상

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,36 +1,35 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { type Coordinate, useGeo } from '@/hooks/useGeo';
 
-// MapboxMap은 클라이언트 컴포넌트지만, 부모가 상태 다 관리
+
 const MapboxMap = dynamic(() => import('@/components/MapboxMap'));
 
-export const MapPage = () => {
-  const { location: currentLocation, error: geoError } = useGeo();
+const MapSkeleton = () => (<div>로딩중...</div>)
+
+const MapPage = () => {
+  const { location: currentLocation } = useGeo();
   const [markers, setMarkers] = useState<Coordinate[]>([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loadMarkers = async () => {
       // const data = await fetchLocations();
       // setMarkers(data);
-      setLoading(false);
     };
     loadMarkers();
   }, []);
 
-  if (geoError) return <p>{geoError}</p>;
-  if (!currentLocation) return <p>현재 위치를 가져오는 중...</p>;
-  if (loading) return <p>마커 데이터를 불러오는 중...</p>;
 
   return (
     <main className="relative w-full height-without-layout">
-      <MapboxMap
-        markers={markers}
-        currentLocation={currentLocation}
-      />
+      <Suspense fallback={<MapSkeleton />}>
+        {currentLocation && <MapboxMap
+          markers={markers}
+          currentLocation={currentLocation}
+        />}
+      </Suspense>
     </main>
   );
 };
