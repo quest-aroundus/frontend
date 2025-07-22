d498608e9fdeb313b5a61648e08ee5c31e2b9fff
EunJin Kim <eunjin9639@gmail.com>
2025-07-21 22:19:20 -0400

    🐎 eslint: 코드 스타일 일관성을 위해 따옴표를 통일하고 포맷 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,33 +10,36 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
-    "rules": {
-      "indent": ["error", 2],
-      "no-unused-vars": [
-        "warn",
+    'rules': {
+      'indent': [
+        'error',
         {
-          "argsIgnorePattern": "^_",
-          "varsIgnorePattern": "^_",
-          "caughtErrorsIgnorePattern": "^_"
-        }
+          indent: 2,
+          switchCase: true,
+        },
       ],
-      "react/no-unescaped-entities": "off",
-      "react/react-in-jsx-scope": "off",
-      "@next/next/no-html-link-for-pages": [
-        "error",
-        "./src/pages/"
-      ],
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
+      'no-unused-vars': [
+        'warn',
         {
-          "argsIgnorePattern": "^_",
-          "varsIgnorePattern": "^_"
-        }
-      ]
-    }
-  }
+          'argsIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+          'caughtErrorsIgnorePattern': '^_',
+        },
+      ],
+      'react/no-unescaped-entities': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@next/next/no-html-link-for-pages': ['error', './src/pages/'],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          'argsIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
