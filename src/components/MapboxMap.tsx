8ca1151f122baa03a9283b0a7c032d3a81f2870e
EunJin Kim <eunjin9639@gmail.com>
2025-07-22 21:56:49 -0400

    🐎 수정: MapboxMap에서 마커 클릭 시 선택된 ID 상태 관리 및 카드 컴포넌트에 선택된 ID 전달 기능 추가

--- src/components/MapboxMap.tsx
+++ src/components/MapboxMap.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import type { Coordinate } from '@/hooks/useGeo';
@@ -18,28 +18,44 @@ mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN!;
 const MapboxMap = ({ markers, currentLocation }: MapboxProps) => {
   const mapRef = useRef<mapboxgl.Map | null>(null);
   const mapContainerRef = useRef<HTMLDivElement>(null);
+  const [selectedId, setSelectedId] = useState<number | null>(null);
 
+  const zoomLevel = 13;
   useEffect(() => {
-    if (!mapRef.current) {
-      mapRef.current = new mapboxgl.Map({
-        container: mapContainerRef.current!,
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [currentLocation.longitude, currentLocation.latitude],
-        zoom: 13,
-      });
-
-      // 🔵 API 마커들 찍기
-      markers.forEach((marker) => {
-        new mapboxgl.Marker({ color: 'blue' })
-          .setLngLat([marker.location.longitude, marker.location.latitude])
-          .addTo(mapRef.current!);
-      });
-
-      // 🔴 현재 위치 마커
-      new mapboxgl.Marker({ color: 'red' })
-        .setLngLat([currentLocation.longitude, currentLocation.latitude])
-        .addTo(mapRef.current!);
+    if (mapRef.current) {
+      return;
     }
+
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current!,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [currentLocation.longitude, currentLocation.latitude],
+      zoom: zoomLevel,
+    });
+
+    // 🔴 현재 위치 마커
+    new mapboxgl.Marker({ color: 'red' })
+      .setLngLat([currentLocation.longitude, currentLocation.latitude])
+      .addTo(mapRef.current!);
+
+    // 🔵 API 마커들 찍기
+    markers.forEach((marker) => {
+      const mapboxMarker = new mapboxgl.Marker({ color: 'blue' })
+        .setLngLat([marker.location.longitude, marker.location.latitude]).setOffset([0, -100])
+        .addTo(mapRef.current!);
+
+      mapboxMarker.getElement().addEventListener('click', () => {
+        setSelectedId(marker.id);
+      });
+
+      mapboxMarker.getElement().addEventListener('mouseenter', () => {
+        mapboxMarker.getElement().style.cursor = 'pointer';
+      });
+      mapboxMarker.getElement().addEventListener('mouseleave', () => {
+        mapboxMarker.getElement().style.cursor = '';
+      });
+    });
+    // 마우스 커서 변경
   }, []);
 
   // 📍 버튼 클릭 시 현재 위치로 이동
@@ -47,26 +63,42 @@ const MapboxMap = ({ markers, currentLocation }: MapboxProps) => {
     if (mapRef.current) {
       mapRef.current.flyTo({
         center: [currentLocation.longitude, currentLocation.latitude],
-        zoom: 14,
+        zoom: zoomLevel,
         essential: true,
       });
     }
   };
 
+  const handleMarkerClick = (marker: Event) => {
+    setSelectedId(marker.id);
+    mapRef.current?.flyTo({
+      center: [marker.location.longitude, marker.location.latitude],
+      offset: [0, -100],
+      zoom: zoomLevel,
+    });
+  }
+
+  const floatingClassName = 'absolute z-50'
+
   return (
     <>
       <div ref={mapContainerRef} className='w-full h-full' />
-      <section className='absolute bottom-5 right-5 z-50 w-full h-full inline-flex flex-col items-end justify-end gap-5'>
-        <button
-          onClick={moveToCurrentLocation}
-          className=' bg-white rounded-[0.625rem] h-14 w-14 shadow-[0px_0px_20px_0px_#716E90] border border-border_lg inline-flex items-center justify-center'
-        >
-          <CurrentLocationIcon />
-        </button>
+      <button
+        onClick={moveToCurrentLocation}
+        className={`right-5 bottom-[14.5rem] bg-white rounded-[0.625rem] h-14 w-14 shadow-[0px_0px_20px_0px_#716E90] border border-border_lg inline-flex items-center justify-center ${floatingClassName}`}
+      >
+        <CurrentLocationIcon />
+      </button>
+      <section className={`bottom-5 w-full flex overflow-x-auto scroll-smooth ${floatingClassName}`}>
         <div className='flex gap-2.5'>
           {
             markers.map((marker) => (
-              <Card item={marker} key={marker.id} />
+              <Card
+                item={marker}
+                key={marker.id}
+                selectedId={selectedId}
+                onSelect={handleMarkerClick}
+              />
             ))
           }
         </div>
--- src/components/map/Card.tsx
+++ src/components/map/Card.tsx
@@ -1,32 +1,50 @@
-import { Event } from '@/types/event';
+import { useEffect, useMemo, useRef } from 'react';
 import Image from 'next/image';
-import { useMemo } from 'react';
 import EventInfoChip from '../common/EventInfoChip';
 import LocationIcon from '@/app/_assets/LocationIcon';
+import type { Event } from '@/types/event';
 
 interface CardProps {
-  item: Event
+  item: Event;
+  selectedId: number | null
+  onSelect: (item: Event) => void
 }
 
-const Card = ({ item }: CardProps) => {
+const Card = ({ item, selectedId, onSelect }: CardProps) => {
   const useFormatDate = (date: string) => useMemo(() => {
     return new Date(date).toLocaleDateString('ko-KR').replace(/ /g, '').slice(0, -1);
   }, [date]);
 
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // 부모에서 내려온 selectedId가 이 카드의 id일 때
+    if (selectedId === item.id && cardRef.current) {
+      cardRef.current.scrollIntoView({
+        behavior: "smooth",
+        inline: "center", // 부모가 가로 스크롤일 때 가운데로 이동
+        block: "nearest", // 세로 스크롤일 땐 이 옵션
+      });
+    }
+  }, [selectedId, item.id]); // selectedId가 바뀔 때마다 체크
+
   return (
-    // border border-sub_b
-    <aside className='flex bg-white w-[22.25rem] rounded-[10px] p-4 shadow-[0px_0px_10px_0px_#00000040]'>
-      <div>
+    <aside
+      onClick={() => onSelect(item)}
+      ref={cardRef}
+      className={`flex bg-white w-[22.25rem] rounded-[10px] p-4 shadow-[0px_0px_10px_0px_#00000040] ${item.id === selectedId ? 'border border-sub_b' : ''}`}
+    >
+      <div className='mr-2.5'>
         <time className='text-main_b text-xs'>{useFormatDate(item.start_dt)}~{useFormatDate(item.end_dt)}</time>
         <h1 className='text-text_b font-bold text-xl leading-6'>{item.title}</h1>
-        <span className='inline-block text-text_b leading-[1.187rem]'>{item.description}</span>
+        <span className='line-clamp-2 text-text_b leading-[1.187rem]'>{item.description}</span>
         <div className='flex gap-2 mt-2.5'>
           <EventInfoChip type='category' value={item.categoryId} />
           <EventInfoChip type='eventSize' value={item.eventSize} />
         </div>
         <div className='inline-flex items-center gap-1 mt-1'>
           <LocationIcon className='text-main_b' />
-          <address className='text-text_b text-xs not-italic'>{item.location.address}</address>
+          <address title={item.location.address} className='line-clamp-1 text-text_b text-xs not-italic'>{item.location.address}</address>
         </div>
       </div>
       <Image
@@ -34,6 +52,7 @@ const Card = ({ item }: CardProps) => {
         alt={item.title}
         width={100}
         height={160}
+        priority
         className='w-[6.25rem] h-40 object-fill rounded-[0.625rem]'
       />
     </aside>
