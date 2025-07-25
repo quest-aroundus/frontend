3ad572ac48d6c7f1651894a209a4fbb1452ef684
EunJin Kim <eunjin9639@gmail.com>
2025-07-24 20:47:49 -0400

    🐎 수정: CalendarPage에서 FullCalendar로 변경하고 다중 월 보기 기능 추가, MapboxMap 컴포넌트를 새로 생성하여 현재 위치 및 API 마커 표시 기능 구현, NotFound 관련 파일 삭제

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,8 +1,23 @@
-import Calendar from '@/components/calendar/Calendar';
+'use client';
+
+import { useEvents } from '@/hooks/queries/useEvents';
+import FullCalendar from '@fullcalendar/react';
+import multiMonthPlugin from '@fullcalendar/multimonth'
+
 
 const CalendarPage = () => {
+  const { data: events } = useEvents();
+
   return <main className='height-without-layout'>
-    <Calendar />
+    <FullCalendar
+      plugins={[multiMonthPlugin]}
+      initialView='multiMonthYear'
+      height='100%'
+      fixedWeekCount={false}
+      viewClassNames='flex-col nowrap items-center'
+      eventClassNames='text-red-500'
+      eventDisplay='block'
+    />
   </main>;
 };
 
--- src/app/not-found/layout.tsx
+++
@@ -1,7 +0,0 @@
-const NotFoundLayout = ({
-  children,
-}: Readonly<{ children: React.ReactNode }>) => {
-  return <>{children}</>;
-};
-
-export default NotFoundLayout;
