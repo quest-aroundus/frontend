5ee5ecdc914e97ff5aa1c12b11c6cc806a8ff199
EunJin Kim <eunjin9639@gmail.com>
2025-08-01 19:13:38 -0400

    🐎 수정: CalendarPage에 FullCalendar 및 다중 월 보기 기능 추가, mockData 이벤트 추가

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,12 +1,93 @@
 'use client';
 
 import { useEvents } from '@/hooks/queries/useEvents';
+import type { Event } from '@/types/event';
 import FullCalendar from '@fullcalendar/react';
 import multiMonthPlugin from '@fullcalendar/multimonth'
 
-
+const mockData: Event[] = [{
+  id: 1,
+  title: 'Sample Event',
+  description: 'This is a sample event description.',
+  start_dt: new Date().toISOString(),
+  end_dt: new Date(new Date().getTime() + 360000000).toISOString(),
+  location: {
+    latitude: 37.5665,
+    longitude: 126.978,
+    address: 'Seoul, South Korea',
+  },
+  categoryId: 1,
+  eventSize: 'large',
+  status: 'active',
+  thumbnailUrl: 'https://example.com/thumbnail.jpg',
+},
+{
+  id: 2,
+  title: 'Another Event',
+  description: 'This is another sample event description.',
+  start_dt: new Date(new Date().getTime() + 86400000).toISOString(),
+  end_dt: new Date(new Date().getTime() + 864000000).toISOString(),
+  location: {
+    latitude: 37.5665,
+    longitude: 126.978,
+    address: 'Seoul, South Korea',
+  },
+  categoryId: 2,
+  eventSize: 'large',
+  status: 'active',
+  thumbnailUrl: 'https://example.com/thumbnail.jpg',
+},
+{
+  id: 3,
+  title: 'Same Event',
+  description: 'This is another sample event description.',
+  start_dt: new Date(new Date().getTime() + 86400000).toISOString(),
+  end_dt: new Date(new Date().getTime() + 864000000).toISOString(),
+  location: {
+    latitude: 37.5665,
+    longitude: 126.978,
+    address: 'Seoul, South Korea',
+  },
+  categoryId: 2,
+  eventSize: 'large',
+  status: 'active',
+  thumbnailUrl: 'https://example.com/thumbnail.jpg',
+},
+{
+  id: 4,
+  title: 'Same2 Event',
+  description: 'This is another sample event description.',
+  start_dt: new Date(new Date().getTime() + 86400000).toISOString(),
+  end_dt: new Date(new Date().getTime() + 864000000).toISOString(),
+  location: {
+    latitude: 37.5665,
+    longitude: 126.978,
+    address: 'Seoul, South Korea',
+  },
+  categoryId: 2,
+  eventSize: 'large',
+  status: 'active',
+  thumbnailUrl: 'https://example.com/thumbnail.jpg',
+},
+]
 const CalendarPage = () => {
   const { data: events } = useEvents();
+
+  const convertToCalendarEvent = (event: Event) => ({
+    id: String(event.id),
+    title: event.title,
+    start: event.start_dt,
+    end: event.end_dt,
+    extendedProps: {
+      description: event.description,
+      location: event.location,
+      categoryId: event.categoryId,
+      eventSize: event.eventSize,
+      status: event.status,
+      thumbnailUrl: event.thumbnailUrl,
+      imageUrls: event.imageUrls,
+    }
+  });
 
   return <main className='height-without-layout'>
     <FullCalendar
@@ -14,9 +95,10 @@ const CalendarPage = () => {
       initialView='multiMonthYear'
       height='100%'
       fixedWeekCount={false}
-      viewClassNames='flex-col nowrap items-center'
+      viewClassNames='flex-col !flex-nowrap items-center'
       eventClassNames='text-red-500'
-      eventDisplay='block'
+      events={events.concat(mockData)?.map(convertToCalendarEvent) || []}
+      displayEventTime={false}
     />
   </main>;
 };
