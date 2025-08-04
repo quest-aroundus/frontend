f3628e15b7f6ebb9519f0b76173d4a5d78d3e462
allyes13 <kimallyes13@gmail.com>
2025-08-04 01:36:10 -0400

    ✨ feat: 이벤트 상세 컴포넌트 화면

--- src/components/event/EventDetail.tsx
+++ src/components/event/EventDetail.tsx
@@ -3,20 +3,52 @@
 import { Suspense } from 'react';
 import { useEvent } from '@/hooks/queries/useEvent';
 import { EventDetailImages } from './EventDetailImages';
+import EventInfoChip from '../common/EventInfoChip';
+import EventDetailMetaInfo from './EventDetailMetaInfo';
+import EventDetailMapInfo from './EventDetailMapInfo';
 
 const EventDetailSkeleton = () => {
   return <div>Loading...</div>;
+};
+
+const EventDetailItem = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='flex flex-col gap-5 h-fit bg-white px-5 py-3'>
+      {children}
+    </div>
+  );
 };
 
 const EventDetail = ({ id }: { id: string }) => {
   const { data: event } = useEvent(id);
 
   return (
-    <div>
-      <EventDetailImages imageUrls={event.imageUrls} />
-      <div>이벤트 상세 - {event.title}</div>
-      <div>이벤트 상세 정보가 여기에 표시됩니다.</div>
-      <div>{JSON.stringify(event)}</div>
+    <div className='flex flex-col gap-[0.625rem] h-fit bg-bg'>
+      <EventDetailItem>
+        <EventDetailImages imageUrls={event.imageUrls} />
+
+        <div className='flex flex-col gap-2'>
+          <div className='text-xl font-semibold'>{event.title}</div>
+        </div>
+
+        <div className='flex gap-2'>
+          <EventInfoChip type='category' value={event.categoryId} />
+          <EventInfoChip type='eventSize' value={event.eventSize} />
+        </div>
+
+        <EventDetailMetaInfo event={event}>
+          <EventDetailMetaInfo.Date />
+          <EventDetailMetaInfo.Location />
+        </EventDetailMetaInfo>
+      </EventDetailItem>
+
+      <EventDetailItem>
+        <div className='flex flex-col gap-2'>{event.description}</div>
+      </EventDetailItem>
+
+      <EventDetailItem>
+        <EventDetailMapInfo event={event} />
+      </EventDetailItem>
     </div>
   );
 };
