b41146f4790fd9fef0bba7a79f2daeed5370b3da
allyes13 <kimallyes13@gmail.com>
2025-08-03 23:53:44 -0400

    ✨ feat: 상세 이미지 캐로셀 구현

--- src/components/event/EventDetail.tsx
+++ src/components/event/EventDetail.tsx
@@ -2,6 +2,7 @@
 
 import { Suspense } from 'react';
 import { useEvent } from '@/hooks/queries/useEvent';
+import { EventDetailImages } from './EventDetailImages';
 
 const EventDetailSkeleton = () => {
   return <div>Loading...</div>;
@@ -9,8 +10,10 @@ const EventDetailSkeleton = () => {
 
 const EventDetail = ({ id }: { id: string }) => {
   const { data: event } = useEvent(id);
+
   return (
     <div>
+      <EventDetailImages imageUrls={event.imageUrls} />
       <div>이벤트 상세 - {event.title}</div>
       <div>이벤트 상세 정보가 여기에 표시됩니다.</div>
       <div>{JSON.stringify(event)}</div>
