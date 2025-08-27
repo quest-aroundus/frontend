f8b29eb617f6f50a3b0b05b273e367cd6fd1b66c
EunJin Kim <eunjin9639@gmail.com>
2025-08-26 22:20:28 -0400

    feat: 서비스 약관 및 개인정보 보호정책 페이지 추가

---
+++ src/app/settings/location/page.tsx
@@ -0,0 +1,12 @@
+import { locationTerms } from '@/constants/setting';
+
+const ServiceTermPage = () => {
+  return (
+    <main className='shadow-[0px_10px_34px_0px_#0000000F] bg-white p-5 mt-3'>
+      {locationTerms.split('\n').map((line, idx) => (
+        <p key={idx}>{line}</p>
+      ))}
+    </main>
+  );
+};
+export default ServiceTermPage;
